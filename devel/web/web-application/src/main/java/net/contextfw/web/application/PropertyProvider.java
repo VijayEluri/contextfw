89d4dbb4cc70781b87111a07591d37b0bc851b9a
marko <marko@marko-laptop.(none)>
2011-06-08 17:02:50 +0300

    Added javadoc

--- devel/web/web-application/src/main/java/net/contextfw/web/application/PropertyProvider.java
+++ devel/web/web-application/src/main/java/net/contextfw/web/application/PropertyProvider.java
@@ -2,6 +2,16 @@ package net.contextfw.web.application;
 
 import java.util.Properties;
 
+/**
+ * This interface defines where system properties are read.
+ * 
+* <p>
+ *  An XSL-post-processor can be set through 
+ *  {@link net.contextfw.web.application.properties.Properties}.
+ * </p>
+ * 
+ * @see net.contextfw.web.application.properties.Properties#PROPERTY_PROVIDER
+ */
 public interface PropertyProvider {
     
     Properties get();
--- devel/web/web-application/src/main/java/net/contextfw/web/application/SystemPropertyProvider.java
+++ devel/web/web-application/src/main/java/net/contextfw/web/application/SystemPropertyProvider.java
@@ -2,6 +2,13 @@ package net.contextfw.web.application;
 
 import java.util.Properties;
 
+/**
+ * The default property provider.
+ *
+ * <p>
+ *  Returns properties from system properties
+ * </p>
+ */
 public class SystemPropertyProvider implements PropertyProvider {
 
     @Override
