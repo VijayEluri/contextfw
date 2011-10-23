84d6fa7cef35e96d9d1e69b01c69a406124175e6
marko <marko@marko-laptop.(none)>
2011-10-23 16:02:46 +0300

    Updated benchmark

--- devel/benchmark/src/main/java/net/contextfw/benchmark/MyApplicationModule.java
+++ devel/benchmark/src/main/java/net/contextfw/benchmark/MyApplicationModule.java
@@ -14,13 +14,13 @@ public class MyApplicationModule extends AbstractModule {
         Configuration props = Configuration.getDefaults()
           .add(Configuration.RESOURCE_PATH, "net.contextfw.benchmark")
           .add(Configuration.VIEW_COMPONENT_ROOT_PACKAGE, "net.contextfw.benchmark.views")
-          .set(Configuration.DEVELOPMENT_MODE, true)
+          .set(Configuration.DEVELOPMENT_MODE, false)
           .set(Configuration.XML_PARAM_NAME, "xml")
           .set(Configuration.MAX_INACTIVITY.inHoursAndMins(1, 0))
           .set(Configuration.LIFECYCLE_LISTENER.as(MyLifecycleListener.class))
           .set(Configuration.LOG_XML, false);
        
-        install( new WebApplicationModule(props));
+        install(new WebApplicationModule(props));
         install(Jsr250.newJsr250Module());
     }    
 }
