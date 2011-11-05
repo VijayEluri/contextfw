b0f30771599ee338a23b2deda78280f6596569ef
marko <marko@marko-laptop.(none)>
2011-11-05 15:12:06 +0200

    Refactored naming

--- devel/web/web-application/src/main/java/net/contextfw/web/application/WebApplicationException.java
+++ devel/web/web-application/src/main/java/net/contextfw/web/application/WebApplicationException.java
@@ -24,22 +24,23 @@ import java.lang.reflect.Method;
  * A general exception thrown by application
  */ 
 public class WebApplicationException extends RuntimeException {
+    
     private static final long serialVersionUID = -3864752109086700032L;
 
     public WebApplicationException() {
         super();
     }
 
-    public WebApplicationException(String arg0, Throwable arg1) {
-        super(arg0, arg1);
+    public WebApplicationException(String msg, Throwable cause) {
+        super(msg, cause);
     }
 
     public WebApplicationException(String msg) {
         super(msg);
     }
 
-    public WebApplicationException(Throwable arg0) {
-        super(arg0);
+    public WebApplicationException(Throwable cause) {
+        super(cause);
     }
     
     public WebApplicationException(Class<?> cl, String msg, Throwable cause) {
