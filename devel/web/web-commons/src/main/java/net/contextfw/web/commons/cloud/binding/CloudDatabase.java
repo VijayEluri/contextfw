947225391cf26c5f3a6f5b1226e2cac34fcc6093
marko <marko@marko-laptop.(none)>
2011-11-08 21:05:19 +0200

    Moved annotation

--- devel/web/web-commons/src/main/java/net/contextfw/web/commons/cloud/binding/CloudDatabase.java
+++ devel/web/web-commons/src/main/java/net/contextfw/web/commons/cloud/binding/CloudDatabase.java
@@ -1,4 +1,4 @@
-package net.contextfw.web.commons.cloud;
+package net.contextfw.web.commons.cloud.binding;
 
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
