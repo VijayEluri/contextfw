6d39a45aa44cfa8908a1a36e6f88d3d2d6c37da4
Marko Lavikainen <marko.lavikainen@netkoti.fi>
2012-03-24 18:54:16 +0200

    Fix: @Provided-annotation missing

--- devel/web/web-commons/src/main/java/net/contextfw/web/commons/i18n/LocaleMessageImpl.java
+++ devel/web/web-commons/src/main/java/net/contextfw/web/commons/i18n/LocaleMessageImpl.java
@@ -3,11 +3,14 @@ package net.contextfw.web.commons.i18n;
 import net.contextfw.web.application.component.Buildable;
 import net.contextfw.web.application.component.CustomBuild;
 import net.contextfw.web.application.component.DOMBuilder;
+import net.contextfw.web.application.scope.Provided;
 
 @Buildable(wrap=false)
 class LocaleMessageImpl implements LocaleMessage {
 
+    @Provided
     private final LocaleServiceImpl service;
+    
     private final String name;
     
     LocaleMessageImpl(LocaleServiceImpl service, String name) {
