87972053c640524b5eb5d84cfd05c986f685c21b
Marko Lavikainen <marko.lavikainen@netkoti.fi>
2012-04-22 13:34:56 +0300

    Added some javadocs

--- devel/web/web-commons/src/main/java/net/contextfw/web/commons/async/AsyncConf.java
+++ devel/web/web-commons/src/main/java/net/contextfw/web/commons/async/AsyncConf.java
@@ -4,16 +4,25 @@ import net.contextfw.web.application.configuration.Configuration;
 import net.contextfw.web.application.configuration.SettableProperty;
 import net.contextfw.web.commons.minifier.MinifierConf;
 
-
+/**
+ * Provides configuration for Async
+ */
 public final class AsyncConf {
     
     private AsyncConf() {
     }
     
+    /**
+     * The mode to be used in asynchronous functionality. Only Jetty and Tomcat are currently
+     * supported 
+     */
     public enum AsyncMode {
         JETTY, TOMCAT, OTHER, NONE
     }
     
+    /**
+     * Informs if web sockets can be enabled on server side
+     */
     public static final SettableProperty<Boolean> WEB_SOCKET_ENABLED = 
             Configuration.createProperty(Boolean.class, 
                     MinifierConf.class.getName() + ".webSocketEnabled");
